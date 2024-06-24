e9fd98ec79b8bc10e630255c5bbd512b4657e662
arctic1106 <arcticsoft@outlook.com>
2024-06-24 03:07:52 +0200

    Refactors.

--- src/main/java/com/arcticsoft/Application.java
+++ src/main/java/com/arcticsoft/Application.java
@@ -1,11 +1,15 @@
 package com.arcticsoft;
 
 public class Application {
+    
     public static void main(String[] args) throws Exception {
-        var numberOfPages = 2;
-        if (args.length > 0) {
-            numberOfPages = Integer.parseInt(args[0]);
+        System.setProperty("java.util.logging.config.file", "src/main/resources/logging.properties"); 
+        var fromPage = 1;
+        var toPage = 200;
+        if (args.length == 2) {
+            fromPage = Integer.parseInt(args[0]);
+            toPage = Integer.parseInt(args[1]);
         }
-        new Service().downloadNPages(numberOfPages);
+        ImageService.downloadPages(fromPage, toPage, true);
     }
 }
